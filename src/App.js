3ea60b6ac84a395007568007205c74f33550a914
Abdul Majid <helloabdulmajid@gmail.com>
2022-08-08 11:53:40 +0530

    facebook login complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
 
+const responseFacebook = (response) => {
+  console.log("login Result" + response);
+}
+
+const componentClicked=(data)=>{
+    console.log(data)
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login With facebook </h1>
+      <FacebookLogin
+    appId="9733396126035841519"
+    autoLoad={true}
+    fields="name,email,picture"
+    onClick={componentClicked}
+    callback={responseFacebook} />,
     </div>
   );
 }
